c606644ac5c494bb0c974ceb9e1ae22b5028c52d
Geoff <geoffreylaporte@gmail.com>
2020-12-01 16:55:35 -0600

    landing page updated for desktop

--- client/src/screens/Landing/Landing.jsx
+++ client/src/screens/Landing/Landing.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import Layout from '../../components/shared/Layout/Layout'
+import './Landing.css'
 
 function Landing(props) {
   return (
     <Layout>
       <div className='content-container'>
+        <div className='hero-container'>
         <p className='hero-text'>Achieve your dreams before the call of the grave</p>
         <button className='global-button'>Join Today</button>
+        </div>
       </div>
     </Layout>
   );
