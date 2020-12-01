ef8e5299065d4b2f435ab747d2bdc079cf818452
Geoff <geoffreylaporte@gmail.com>
2020-12-01 17:51:14 -0600

    updated basic styling and added a form for login

--- client/src/screens/Login/Login.jsx
+++ client/src/screens/Login/Login.jsx
@@ -1,10 +1,44 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Layout from '../../components/shared/Layout/Layout';
+import './Login.css'
 
 function Login(props) {
+  const [formData, setFormData] = useState({
+    username: "",
+    password: ""
+  })
+
+  const handleChange = (e) => {
+    const { name, value } = e.target
+    setFormData(prevState => ({
+      ...prevState,
+      [name]: value
+    }))
+  }
+
   return (
     <Layout>
       <div className='content-container'>
+        <p className='hero-text'>Welcome Back</p>
+        <p className='login-text'>Thank you for visit our site, we are excited to have you be apart of our community of dreamers</p>
+        <form className='login-form' onSubmit={(e) => {
+          e.preventDefault()
+          props.handleLogin(formData)
+        }}>
+          <label htmlFor='name'>Username</label>
+          <input
+            type='text'
+            name='name'
+            placeholder='Enter username'
+          />
+          <label htmlFor='password'>Password</label>
+          <input
+            type='password'
+            name='password'
+            placeholder='Enter password'
+          />
+          <button>Submit</button>
+        </form>
       
       </div>
     </Layout>
