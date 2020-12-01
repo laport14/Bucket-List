241bef52838aedec355a8ca61d8c04a28806965e
Geoff <geoffreylaporte@gmail.com>
2020-12-01 14:07:33 -0600

    added files and structure to front end

---
+++ client/src/screens/AddActivity/AddActivity.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function AddActivity(props) {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default AddActivity;
---
+++ client/src/screens/EditActivity/EditActivity.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function EditActivity(props) {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default EditActivity;
