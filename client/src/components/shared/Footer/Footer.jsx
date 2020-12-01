78af5c28383864055fb238ccc8088f0a4f535d32
Geoff <geoffreylaporte@gmail.com>
2020-12-01 16:24:56 -0600

    updated layout styling for fixed footer position and added logo

--- client/src/components/shared/Footer/Footer.jsx
+++ client/src/components/shared/Footer/Footer.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import './Footer.css'
 
 function Footer(props) {
   return (
-    <div>
-      
+    <div className="footer-container">
+      <div className='footer-flex-container'>
+
+      </div>
     </div>
   );
 }
--- client/src/components/shared/Nav/Nav.jsx
+++ client/src/components/shared/Nav/Nav.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import './Nav.css'
+import Logo from '../../../assets/logo3.png'
 
 function Nav(props) {
   return (
-    <div>
-      
+    <div className='nav-bar'>
+      <div className='nav-contents'>
+        <img className='nav-logo' src={Logo} alt='webpage logo'/>
+        <div className='nav-button-div'>
+          <button className='nav-button'>Login</button>
+          <button className='nav-button'>Register</button>
+        </div>
+      </div>
     </div>
   );
 }
