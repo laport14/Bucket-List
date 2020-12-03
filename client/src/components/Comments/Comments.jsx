00d28406d7a9a7f394b7ab52495d2c69a663b336
Geoff <geoffreylaporte@gmail.com>
2020-12-03 09:21:05 -0600

    stored comment to state in activitydetail getting comment now, have to add more seed data for more comments

--- client/src/components/Comments/Comments.jsx
+++ client/src/components/Comments/Comments.jsx
@@ -3,7 +3,8 @@ import React from 'react';
 function Comments(props) {
   return (
     <div>
-      
+      <p>{props.username}</p>
+      <p>{props.comment}</p>
     </div>
   );
 }
--- client/src/screens/ActivityDetail/ActivityDetail.jsx
+++ client/src/screens/ActivityDetail/ActivityDetail.jsx
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import {Link} from 'react-router-dom'
 import { useParams } from 'react-router-dom'
+import Comments from '../../components/Comments/Comments';
 import {getOneActivity} from '../../services/activities'
 
 function ActivityDetail(props) {
   const [activity, setActivity] = useState(null)
+  const [comments, setComments] = useState([])
 
   const { id } = useParams()
 
@@ -12,6 +14,7 @@ function ActivityDetail(props) {
     const fetchActivityItem = async () => {
       const activityData = await getOneActivity(id)
       setActivity(activityData)
+      setComments(activityData.comments)
     }
     fetchActivityItem()
   }, [id])
@@ -26,8 +29,17 @@ function ActivityDetail(props) {
   //   set
   // }
 
+  const commentsJSX = comments.map((comments, index) => (
+    <Comments 
+      comment={comments.comment}
+      username={comments.user.username}
+      key={index}
+    />
+  ))
+
   return (
     <div>
+      <div>
       <img src={activity?.image_url} alt='activity'/>
       <h2>{activity?.name}</h2>
       <p>{activity?.description}</p>
@@ -35,6 +47,8 @@ function ActivityDetail(props) {
       <p>{activity?.location}</p>
       <Link className='edit-link' to={`/activities/${id}/edit`}><button>Edit</button></Link>
       <button onClick={() => props.handleDelete(activity.id)}>Delete</button>
+      </div>
+      <div>{commentsJSX}</div>
       
     </div>
   );
