c69cf63a86ed71a03bd064b7a0b7b88907945de9
Geoff <geoffreylaporte@gmail.com>
2020-12-05 18:33:59 -0600

    updated styling for nav bar and all activities page

--- client/src/components/shared/Nav/Nav.jsx
+++ client/src/components/shared/Nav/Nav.jsx
@@ -15,17 +15,17 @@ function Nav(props) {
         <div className='nav-button-div'>
           {currentUser ?
             <>
-              <Link to='/activities' style={{textDecoration: 'none', color: '#ffffff'}}>
-                <p className='activity-nav-text'>All Activities</p>
+              <p className='welcome-user'> Welcome {currentUser.username}</p>
+              <Link to='/activities'>
+                <button className='nav-activity-button'>All Activities</button>
               </Link>
-              <p>{currentUser.username}</p>
               <button onClick={handleLogout} className='nav-button'>Logout</button>
             </>
             : <>
-              <Link to='/login' style={{margin: '2%'}}>
-              <button className='nav-button'>Login</button>
+              <Link to='/login'>
+              <button className='nav-login-button'>Login</button>
               </Link>
-              <Link to='/register' style={{margin: '2%'}}>
+              <Link to='/register'>
                 <button className='nav-button'>Register</button>
               </Link>
               </>
--- client/src/screens/ActivityDetail/ActivityDetail.jsx
+++ client/src/screens/ActivityDetail/ActivityDetail.jsx
@@ -55,29 +55,36 @@ function ActivityDetail(props) {
       <img className='activity-detail-image' src={activity?.image_url} alt='activity'/>
       </div>
       <div className='activity-descriptive-content'>
-      <h2>{activity?.name}</h2>
-      <p>{activity?.description}</p>
-      <p>{activity?.price}</p>
-      <p>{activity?.location}</p>
-      <Link className='edit-link' to={`/activities/${id}/edit`}><button>Edit</button></Link>
-      <button onClick={() => props.handleDelete(activity.id)}>Delete</button>
+      <h2 className='activity-name'>{activity?.name}</h2>
+      <p className='descriptive-text'>{activity?.description}</p>
+      <p className='descriptive-text'>Price: {activity?.price}</p>
+          <p className='descriptive-text'>Location: {activity?.location}</p>
+          <div>
+      <Link className='edit-link' to={`/activities/${id}/edit`}><button className='edit-button'>Edit</button></Link>
+      <button className='delete-button' onClick={() => props.handleDelete(activity.id)}>Delete</button>
+      </div>
       </div>
     </div>
 
-      <div>
-      <div>{commentsJSX}</div>
+      <div className='comment-container'>
+        <div className='comments-child'>{commentsJSX}</div>
+        <div>
+          <h2 className='comment-heading'>Comment on a bucket list Activity</h2>
         <form onSubmit={(e) => {
           e.preventDefault()
           handleSubmit(id, formData)
-        }}>
+          
+        }}className='form-comments'>
           <textarea
             placeholder="Insert comment here"
             name='comment'
+            className='textarea-detail'
             value={formData.comment}
             onChange={handleChange}
           />
-          <button>Submit</button>
-        </form>
+          <button className='comment-button'>Submit</button>
+          </form>
+        </div>
       </div>
       
       
